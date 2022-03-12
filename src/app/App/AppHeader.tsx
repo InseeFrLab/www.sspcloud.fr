f4784cf54d31ae932bc265bae41919b9d6a08b68
thieryw <williamalexthiery@hotmail.fr>
2022-03-12 23:55:46 +0100

    AppHeader update link and make it collapsible

--- src/app/App/AppHeader.tsx
+++ src/app/App/AppHeader.tsx
@@ -4,92 +4,103 @@ import { useTranslation } from "app/i18n/useTranslations";
 import { useLanguage } from "app/i18n/useLanguage";
 import { routes } from "app/router";
 import { GlHeader } from "gitlanding/GlHeader";
+import { useDomRect } from "powerhooks/useDomRect";
 
 export type Props = {
     className?: string;
+    isRetracted?: boolean;
 };
 
-const useStyles = makeStyles()({
-    "titleWrapper": {
-        "display": "flex",
-        "justifyContent": "center",
-        "alignItems": "center",
-        "textDecoration": "none",
-    },
-    "sspCloudText": {
-        "fontWeight": 500,
-    },
-    "communityText": {
-        "fontWeight": 600,
-    },
-});
 
 export const AppHeader = memo((props: Props) => {
-    const { className } = props;
+    const { className, isRetracted } = props;
 
     const { t } = useTranslation("AppHeader");
     const { language } = useLanguage();
+    const { ref, domRect: { height: headerHeight } } = useDomRect();
 
-    const { classes, theme } = useStyles();
+    const { classes, theme } = useStyles({
+        "isRetracted": isRetracted ?? false,
+        headerHeight
+    });
 
     return (
-        <GlHeader
-            className={className}
-            title={
-                <a className={classes.titleWrapper} {...routes.home().link}>
-                    {(() => {
-                        const out = [
-                            <Text
-                                key={0}
-                                typo="section heading"
-                                className={classes.communityText}
-                            >
-                                {t("community")}
-                            </Text>,
-                            <div key={1} style={{ "width": theme.spacing(3) }} />,
-                            <Text
-                                key={2}
-                                typo="section heading"
-                                className={classes.sspCloudText}
-                                color="focus"
-                            >
-                                SSP Cloud
-                            </Text>,
-                        ];
+        <div className={classes.root} ref={ref}>
+            <GlHeader
+                className={className}
+                title={
+                    <a className={classes.titleWrapper} {...routes.home().link}>
+                        {(() => {
+                            const out = [
+                                <Text
+                                    key={0}
+                                    typo="section heading"
+                                    className={classes.communityText}
+                                >
+                                    {t("community")}
+                                </Text>,
+                                <div key={1} style={{ "width": theme.spacing(3) }} />,
+                                <Text
+                                    key={2}
+                                    typo="section heading"
+                                    className={classes.sspCloudText}
+                                    color="focus"
+                                >
+                                    SSP Cloud
+                                </Text>,
+                            ];
 
-                        if (language === "en") {
-                            return out.reverse();
-                        }
+                            if (language === "en") {
+                                return out.reverse();
+                            }
 
-                        return out;
-                    })()}
-                </a>
-            }
-            links={[
-                {
-                    "label": t("trainings and tutorials"),
-                    "link": routes.documentation().link,
-                },
-                {
-                    "label": t("the onyxia datalab"),
-                    "link": { "href": "https://datalab.sspcloud.fr" },
-                },
-                {
-                    "label": t("our GitLab forge"),
-                    "link": {
+                            return out;
+                        })()}
+                    </a>
+                }
+                links={[
+                    {
+                        "label": t("trainings and tutorials"),
+                        ...routes.documentation().link,
+                    },
+                    {
+                        "label": t("the onyxia datalab"),
+                        "href": "https://datalab.sspcloud.fr",
+                    },
+                    {
+                        "label": t("our GitLab forge"),
                         "href": "https://git.lab.sspcloud.fr/",
                     },
-                },
-                {
-                    "label": t("contribute"),
-                    "link": {
+                    {
+                        "label": t("contribute"),
                         "href": "https://docs.sspcloud.fr/comment-contribuer-1/comment-contribuer",
                     },
-                },
-            ]}
-        />
+                ]}
+            />
+
+        </div>
     );
 });
+
+const useStyles = makeStyles<{ isRetracted: boolean; headerHeight: number }>()(
+    (theme, { isRetracted, headerHeight }) => ({
+        "root": {
+            "transition": "margin-top 250ms",
+            "margin-top": isRetracted ? -(headerHeight + theme.spacing(3)) : 0
+        },
+        "titleWrapper": {
+            "display": "flex",
+            "justifyContent": "center",
+            "alignItems": "center",
+            "textDecoration": "none",
+        },
+        "sspCloudText": {
+            "fontWeight": 500,
+        },
+        "communityText": {
+            "fontWeight": 600,
+        },
+    }));
 
 const links = [
     "trainings and tutorials",
