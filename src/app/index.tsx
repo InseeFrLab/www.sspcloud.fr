3f2879d88dea342737d3ab5c3bc5c4ef6a326935
thieryw <williamalexthiery@hotmail.fr>
2022-03-12 23:49:39 +0100

    wrap App in theme provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,14 @@ import { render } from "react-dom";
 import { I18nProvider } from "./i18n/I18nProvider";
 import { App } from "./App/App";
 import { RouteProvider } from "./router";
+import { ThemeProvider, splashScreen } from "./theme";
 
 render(
     <RouteProvider>
         <I18nProvider>
-            <App />
+            <ThemeProvider splashScreen={splashScreen}>
+                <App />
+            </ThemeProvider>
         </I18nProvider>
     </RouteProvider>,
     document.getElementById("root"),
