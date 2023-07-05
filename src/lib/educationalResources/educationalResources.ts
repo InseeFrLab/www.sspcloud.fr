ad659c840d6c4cae4ae45f2a0bc4e18fd3482f40
avouacr <avouacr@pm.me>
2023-07-05 12:41:51 +0000

    add record linkages tutorials

--- src/lib/educationalResources/educationalResources.ts
+++ src/lib/educationalResources/educationalResources.ts
@@ -2392,4 +2392,53 @@ export const educationalResources: (
       },
     ],
   },
+  {
+    "name": "Appariement de données individuelles",
+    "abstract":
+        "Des tutoriels en Python et en R pour s'initier à l'appariement de données individuelles.",
+    "authors": ["Lucas Malherbe"],
+    "contributors": ["Lucas Malherbe"],
+    "types": ["Notebook Python"],
+    "tags": ["consolidate", "learn"],
+    "category": "training courses in data science",
+    "imageUrl": pythonImgUrl,
+    "parts": [
+        {
+            "name": "Package Record Linkage (Python)",
+            "abstract": "Appariement de données individuelles en Python avec le package Record Linkage",
+            "authors": ["Lucas Malherbe"],
+            "contributors": ["Lucas Malherbe"],
+            "types": ["Notebook Python"],
+            "tags": ["discover", "learn"],
+            "category": "training courses in data science",
+            "imageUrl": pythonImgUrl,
+            "deploymentUrl":
+                "https://datalab.sspcloud.fr/launcher/ide/jupyter-python?autoLaunch=true&init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com%2FInseeFrLab%2Fappariement%2Fmain%2Finit.sh%C2%BB&init.personalInitArgs=%C2%ABrecordLinkage%C2%BB",
+        },
+        {
+            "name": "Package dedupe (Python)",
+            "abstract": "Appariement de données individuelles en Python avec le package dedupe",
+            "authors": ["Lucas Malherbe"],
+            "contributors": ["Lucas Malherbe"],
+            "types": ["Notebook Python"],
+            "tags": ["discover", "learn"],
+            "category": "training courses in data science",
+            "imageUrl": pythonImgUrl,
+            "deploymentUrl":
+                "https://datalab.sspcloud.fr/launcher/ide/jupyter-python?autoLaunch=true&init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com%2FInseeFrLab%2Fappariement%2Fmain%2Finit.sh%C2%BB&init.personalInitArgs=%C2%ABdedupe%C2%BB",
+        },
+        {
+            "name": "Package reclin2 (R)",
+            "abstract": "Appariement de données individuelles en R avec le package reclin2",
+            "authors": ["Lucas Malherbe"],
+            "contributors": ["Lucas Malherbe"],
+            "types": ["Notebook R"],
+            "tags": ["discover", "learn"],
+            "category": "training courses in data science",
+            "imageUrl": rSvgUrl,
+            "deploymentUrl":
+                "https://datalab.sspcloud.fr/launcher/ide/jupyter-r?autoLaunch=true&init.personalInitArgs=%C2%ABreclin2%C2%BB&init.personalInit=%C2%ABhttps%3A%2F%2Fraw.githubusercontent.com%2FInseeFrLab%2Fappariement%2Fmain%2Finit.sh%C2%BB",
+        },
+    ],
+  },
 ];
