e40899371b9e9bbed8d63a0eda2be9520f60dd2e
Joseph Garrone <joseph.garrone.gj@gmail.com>
2024-06-04 21:15:40 +0200

    Add missing file

---
+++ src/tools/prettyPrintDuration.ts
@@ -0,0 +1,16 @@
+
+export function formatDuration(minutes: number): string {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    let result = "";
+
+    if (hours > 0) {
+        result += `${hours}h`;
+    }
+    if (remainingMinutes > 0) {
+        
+        result += remainingMinutes === minutes ? `${remainingMinutes} minutes`: `${remainingMinutes}`;
+    }
+
+    return result;
+}
