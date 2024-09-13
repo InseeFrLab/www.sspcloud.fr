a08dbb8956d4f399383ddba8cb4a1d777a2e7755
linogaliana <lino.galiana@insee.fr>
2024-09-13 14:43:45 +0000

    Add Rgeo courses on training page

--- src/lib/educationalResources/educationalResources.ts
+++ src/lib/educationalResources/educationalResources.ts
@@ -2362,6 +2362,90 @@ export const educationalResources: (
             "https://datalab.sspcloud.fr/launcher/ide/rstudio?autoLaunch=true&name=utilitr&init.personalInit=%C2%ABhttps%3A%2F%2Fminio.lab.sspcloud.fr%2Fpierrelamarche%2Futilitr%2Finit_utilitr.sh%C2%BB&service.image.version=%C2%ABinseefrlab%2Futilitr%3A0.9.0%C2%BB&vault.secret=%C2%AButilitr%2Futilitr%C2%BB",
     },
     {
+        "name": "Introduction à R pour les sciences sociales",
+        "abstract":
+            "Cours d'introduction à R pour des étudiants en sciences sociales quantitatives (économie, géographie, sociologie, etc.) de niveau L3 ou Master.",
+        "authors": ["Lino Galiana"],
+        "types": ["Tutoriel R"],
+        "tags": ["discover", "learn"],
+        "imageUrl": rSvgUrl,
+        "category": "training courses with R",
+        "parts": [
+            {
+                "name": "Introduction aux objets de base de R",
+                "abstract":
+                    "Une introduction à la logique du langage R, à ses principes et aux objets de base",
+                "authors": ["Lino Galiana"],
+                "types": ["Tutoriel R"],
+                "tags": ["discover", "learn"],
+                "category": "training courses with R",
+                "timeRequired": 90,
+                "imageUrl": rSvgUrl,
+                "articleUrl": "https://rgeo.linogaliana.fr/exercises/r-base.html",
+            },            
+            {
+                "name": "Manipuler des données avec le tidyverse",
+                "abstract":
+                    "Une introduction à dplyr pour découvrir comment travailler sur un _dataframe_ avec `R`. Les exemples utilisent les données d'inventaire carbone de l'Ademe et les données Filosofi, une base sur les revenus au niveau communal.",
+                "authors": ["Lino Galiana"],
+                "types": ["Tutoriel R"],
+                "tags": ["discover", "learn"],
+                "category": "training courses with R",
+                "timeRequired": 180,
+                "imageUrl": rSvgUrl,
+                "articleUrl": "https://rgeo.linogaliana.fr/exercises/r-wrangling.html",
+            },            
+            {
+                "name": "Manipuler des données spatiales avec sf",
+                "abstract":
+                    "Un chapitre consacré à la présentation de l'écosystème pour travailler sur des données spatiales avec `R` et aux concepts nécessaires pour cela. Les illustrations s'appuient sur les données d'implantation du Vélib dans l'agglomération parisienne.",
+                "authors": ["Lino Galiana"],
+                "types": ["Tutoriel R"],
+                "tags": ["discover", "learn"],
+                "category": "training courses with R",
+                "timeRequired": 180,
+                "imageUrl": rSvgUrl,
+                "articleUrl": "https://rgeo.linogaliana.fr/exercises/geospatial-wrangling.html",
+            },            
+            {
+                "name": "Produire des représentations graphiques avec R",
+                "abstract":
+                    "Un chapitre consacré à la présentation de ggplot, l'incontournable pour faire des graphiques avec `R`. Les exercices d'application s'appuient sur les données de fréquentation des pistes cyclables parisiennes.",
+                "authors": ["Lino Galiana"],
+                "types": ["Tutoriel R"],
+                "tags": ["discover", "learn"],
+                "category": "training courses with R",
+                "timeRequired": 180,
+                "imageUrl": rSvgUrl,
+                "articleUrl": "https://rgeo.linogaliana.fr/exercises/ggplot.html",
+            },            
+            {
+                "name": "Produire des cartes avec R",
+                "abstract":
+                    "Un chapitre consacré à la cartographie, statique (ggplot, mapsf) et réactive (leaflet). Les exemples permettent de produire des cartes de population à plusieurs échelles",
+                "authors": ["Lino Galiana"],
+                "timeRequired": 180,
+                "types": ["Tutoriel R"],
+                "tags": ["discover", "learn"],
+                "category": "training courses with R",
+                "imageUrl": rSvgUrl,
+                "articleUrl": "https://rgeo.linogaliana.fr/exercises/cartography.html",
+            },            
+            {
+                "name": "Introduction aux publications reproductives avec R et Quarto",
+                "abstract":
+                    "Un chapitre consacré à Quarto, l'écosystème permettant des publications reproductibles mélant de manière harmonieuse code et analyse des résultats.",
+                "authors": ["Lino Galiana"],
+                "types": ["Tutoriel R"],
+                "tags": ["discover", "learn"],
+                "timeRequired": 180,
+                "category": "training courses with R",
+                "imageUrl": rSvgUrl,
+                "articleUrl": "https://rgeo.linogaliana.fr/exercises/quarto.html",
+            },            
+        ]
+        },
+    {
         "name": {
             "fr": "Traitement des données volumineuses avec R",
             "en": "Processing large data sets with R",
