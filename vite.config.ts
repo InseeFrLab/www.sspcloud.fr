267edbc91300c0ac81b6ddb9132165d4de0f7b07
garronej <joseph.garrone@protonmail.com>
2025-09-07 23:07:58 +0200

    Hack to make HMR work

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,6 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    server: {
-        hmr: false,
-    },
     build: {
         sourcemap: true, // enables accurate module sizing
         rollupOptions: {
