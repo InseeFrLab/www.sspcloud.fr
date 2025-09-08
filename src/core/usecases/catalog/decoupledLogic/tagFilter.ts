daec46ba1c3aacdbca6cef0d9124b2498dc54b7b
garronej <joseph.garrone@protonmail.com>
2025-09-08 20:07:27 +0200

    Remove debug log

--- src/core/usecases/catalog/decoupledLogic/tagFilter.ts
+++ src/core/usecases/catalog/decoupledLogic/tagFilter.ts
@@ -2,49 +2,20 @@ import type { EducationalResource } from "core/ports/CatalogData";
 import { removeDuplicates } from "evt/tools/reducers/removeDuplicates";
 import memoize from "memoizee";
 
-let isCacheHit: boolean;
-
-let hitCount = 0;
-let missCount = 0;
-let hitDuration = 0;
-let missDuration = 0;
-
 export function filterMatchingSelectedTags(params: {
     parts: EducationalResource[];
     selectedTags: EducationalResource.Tag[];
 }): EducationalResource[] {
     const { parts, selectedTags } = params;
 
-    isCacheHit = true;
-
-    const start = Date.now();
-
-    const x = filterMatchingSelectedTags_memo(
+    return filterMatchingSelectedTags_memo(
         parts,
         JSON.stringify([...selectedTags].sort()),
     );
-
-    const duration = Date.now() - start;
-
-    if (isCacheHit) {
-        hitDuration += duration;
-        hitCount++;
-    } else {
-        missDuration += duration;
-        missCount++;
-    }
-
-    if (hitCount + missCount === 200) {
-        console.log({ hitCount, missCount, hitDuration, missDuration });
-    }
-
-    return x;
 }
 
 export const filterMatchingSelectedTags_memo = memoize(
     (parts: EducationalResource[], selectedTags_str: string): EducationalResource[] => {
-        isCacheHit = false;
-
         const selectedTags: EducationalResource.Tag[] = JSON.parse(selectedTags_str);
 
         return parts.filter(educationalResource => {
