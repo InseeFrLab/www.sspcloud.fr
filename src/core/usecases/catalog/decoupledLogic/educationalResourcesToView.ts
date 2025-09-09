8a77c8618e58c3754f2089d7779fb7a89f7b9749
garronej <joseph.garrone@protonmail.com>
2025-09-10 00:28:10 +0200

    Memoization where it maters

--- src/core/usecases/catalog/decoupledLogic/educationalResourcesToView.ts
+++ src/core/usecases/catalog/decoupledLogic/educationalResourcesToView.ts
@@ -16,6 +16,7 @@ import { getLocalizedStringId } from "./getLocalizedStringId";
 import { assert, type Equals } from "tsafe/assert";
 import { objectKeys } from "tsafe/objectKeys";
 import { same } from "evt/tools/inDepth/same";
+import memoize from "memoizee";
 
 type LocalizedString = LocalizedString_base<Language>;
 
@@ -33,12 +34,53 @@ export function educationalResourcesToView(params: {
         languageAssumedIfNoTranslation,
         search,
         tagLabelByTagId,
-        selectedTags: selectedTags_arr,
+        selectedTags,
     } = params;
 
-    const selectedTags = new Set(selectedTags_arr);
+    const selectedTags_str = JSON.stringify([...selectedTags].sort());
 
-    const { resolveLocalizedStringDetailed } = (() => {
+    const { resolveLocalizedStringDetailed } = getResolveLocalizedStringDetailed(
+        language,
+        languageAssumedIfNoTranslation,
+    );
+
+    const { path_names, collection, parts } = selected;
+
+    return {
+        header:
+            collection === undefined
+                ? undefined
+                : {
+                      path: path_names.map(resolveLocalizedStringDetailed),
+                      name: resolveLocalizedStringDetailed(collection.name),
+                      imageUrl: collection.imageUrl,
+                      authors: educationalResourceToViewItem(
+                          collection,
+                          resolveLocalizedStringDetailed,
+                          search,
+                          tagLabelByTagId,
+                          selectedTags_str,
+                          languageAssumedIfNoTranslation,
+                      ).authors.map(({ text, langAttrValue }) => ({
+                          langAttrValue,
+                          text: text.charArray.join(""),
+                      })),
+                  },
+        items: parts.map(part =>
+            educationalResourceToViewItem(
+                part,
+                resolveLocalizedStringDetailed,
+                search,
+                tagLabelByTagId,
+                selectedTags_str,
+                languageAssumedIfNoTranslation,
+            ),
+        ),
+    };
+}
+
+const getResolveLocalizedStringDetailed = memoize(
+    (language: Language, languageAssumedIfNoTranslation: Language) => {
         const { resolveLocalizedStringDetailed: base } = createResolveLocalizedString({
             currentLanguage: language,
             fallbackLanguage: "en",
@@ -58,145 +100,112 @@ export function educationalResourcesToView(params: {
             };
 
         return { resolveLocalizedStringDetailed };
-    })();
-
-    const { path_names, collection, parts } = selected;
-
-    return {
-        header:
-            collection === undefined
-                ? undefined
-                : {
-                      path: path_names.map(resolveLocalizedStringDetailed),
-                      name: resolveLocalizedStringDetailed(collection.name),
-                      imageUrl: collection.imageUrl,
-                      authors: educationalResourceToViewItem({
-                          educationalResource: collection,
-                          resolveLocalizedStringDetailed,
-                          search,
-                          tagLabelByTagId,
-                          selectedTags,
-                          languageAssumedIfNoTranslation,
-                      }).authors.map(({ text, langAttrValue }) => ({
-                          langAttrValue,
-                          text: text.charArray.join(""),
-                      })),
-                  },
-        items: parts.map(part =>
-            educationalResourceToViewItem({
-                educationalResource: part,
-                resolveLocalizedStringDetailed,
-                search,
-                tagLabelByTagId,
-                selectedTags,
-                languageAssumedIfNoTranslation,
-            }),
-        ),
-    };
-}
+    },
+);
 
 type ResolveLocalizedStringDetailed = (
     localizedString: LocalizedString,
 ) => TextMaybeNotInAmbientLanguage<string>;
 
-function educationalResourceToViewItem(params: {
-    educationalResource: EducationalResource;
-    resolveLocalizedStringDetailed: ResolveLocalizedStringDetailed;
-    search: string;
-    tagLabelByTagId: Record<EducationalResource.Tag, LocalizedString>;
-    selectedTags: Set<EducationalResource.Tag>;
-    languageAssumedIfNoTranslation: Language;
-}): View.Item {
-    const {
-        educationalResource: er,
-        resolveLocalizedStringDetailed,
-        search,
-        tagLabelByTagId,
-        selectedTags,
-        languageAssumedIfNoTranslation,
-    } = params;
+const educationalResourceToViewItem = memoize(
+    (
+        er: EducationalResource,
+        resolveLocalizedStringDetailed: ResolveLocalizedStringDetailed,
+        search: string,
+        tagLabelByTagId: Record<EducationalResource.Tag, LocalizedString>,
+        selectedTags_str: string,
+        languageAssumedIfNoTranslation: Language,
+    ): View.Item => {
+        const selectedTags = new Set(
+            JSON.parse(selectedTags_str) as EducationalResource.Tag[],
+        );
 
-    const toHighlightableString = (
-        localizedString: LocalizedString,
-    ): TextMaybeNotInAmbientLanguage<StringWithHighlights> => {
-        const { langAttrValue, text } = resolveLocalizedStringDetailed(localizedString);
-        return {
-            langAttrValue: langAttrValue,
-            text: highlightString({ str: text, search }),
+        const toHighlightableString = (
+            localizedString: LocalizedString,
+        ): TextMaybeNotInAmbientLanguage<StringWithHighlights> => {
+            const { langAttrValue, text } =
+                resolveLocalizedStringDetailed(localizedString);
+            return {
+                langAttrValue: langAttrValue,
+                text: highlightString({ str: text, search }),
+            };
         };
-    };
 
-    const common: View.Item.Common = {
-        name: toHighlightableString(er.name),
-        abstract: toHighlightableString(er.abstract),
-        imageUrl: er.imageUrl,
-        authors: collectAuthors_noDuplicateRemoval(er)
-            .map(toHighlightableString)
-            .reduce(
-                ...removeDuplicates<TextMaybeNotInAmbientLanguage<StringWithHighlights>>(
-                    (a, b) => same(a.text.charArray, b.text.charArray),
+        const common: View.Item.Common = {
+            name: toHighlightableString(er.name),
+            abstract: toHighlightableString(er.abstract),
+            imageUrl: er.imageUrl,
+            authors: collectAuthors_noDuplicateRemoval(er)
+                .map(toHighlightableString)
+                .reduce(
+                    ...removeDuplicates<
+                        TextMaybeNotInAmbientLanguage<StringWithHighlights>
+                    >((a, b) => same(a.text.charArray, b.text.charArray)),
                 ),
-            ),
-        lastUpdatedTime: computeLastUpdatedTime(er),
-        tags: collectTags(er).map(tag => ({
-            id: tag,
-            label: toHighlightableString(tagLabelByTagId[tag]),
-            isSelected: selectedTags.has(tag),
-        })),
-        timeRequired: (() => {
-            const inMinute = computeTimeRequestedInMinutes(er);
+            lastUpdatedTime: computeLastUpdatedTime(er),
+            tags: collectTags(er).map(tag => ({
+                id: tag,
+                label: toHighlightableString(tagLabelByTagId[tag]),
+                isSelected: selectedTags.has(tag),
+            })),
+            timeRequired: (() => {
+                const inMinute = computeTimeRequestedInMinutes(er);
 
-            return inMinute === undefined ? undefined : inMinute * 60 * 1_000;
-        })(),
-        availableInLanguages: getAvailableInLanguages({
-            educationalResource: er,
-            languageAssumedIfNoTranslation,
-        }),
-    };
+                return inMinute === undefined ? undefined : inMinute * 60 * 1_000;
+            })(),
+            availableInLanguages: getAvailableInLanguages({
+                educationalResource: er,
+                languageAssumedIfNoTranslation,
+            }),
+        };
 
-    return "parts" in er
-        ? id<View.Item.Collection>({
-              ...common,
-              isCollection: true,
-              pathSegment: getLocalizedStringId(er.name),
-          })
-        : id<View.Item.Resource>({
-              ...common,
-              isCollection: false,
-              articleUrl:
-                  er.articleUrl === undefined
-                      ? undefined
-                      : resolveLocalizedStringDetailed(er.articleUrl).text,
-              deploymentUrl: (() => {
-                  const { deploymentUrl } = er;
+        return "parts" in er
+            ? id<View.Item.Collection>({
+                  ...common,
+                  isCollection: true,
+                  pathSegment: getLocalizedStringId(er.name),
+              })
+            : id<View.Item.Resource>({
+                  ...common,
+                  isCollection: false,
+                  articleUrl:
+                      er.articleUrl === undefined
+                          ? undefined
+                          : resolveLocalizedStringDetailed(er.articleUrl).text,
+                  deploymentUrl: (() => {
+                      const { deploymentUrl } = er;
 
-                  if (deploymentUrl === undefined) {
-                      return undefined;
-                  }
+                      if (deploymentUrl === undefined) {
+                          return undefined;
+                      }
 
-                  if (getIsLocalizedString(deploymentUrl)) {
-                      return resolveLocalizedStringDetailed(deploymentUrl).text;
-                  }
+                      if (getIsLocalizedString(deploymentUrl)) {
+                          return resolveLocalizedStringDetailed(deploymentUrl).text;
+                      }
 
-                  return Object.fromEntries(
-                      objectKeys(deploymentUrl).map(ideName => [
-                          ideName,
-                          resolveLocalizedStringDetailed(deploymentUrl[ideName]).text,
-                      ]),
-                  );
-              })(),
-          });
-}
+                      return Object.fromEntries(
+                          objectKeys(deploymentUrl).map(ideName => [
+                              ideName,
+                              resolveLocalizedStringDetailed(deploymentUrl[ideName]).text,
+                          ]),
+                      );
+                  })(),
+              });
+    },
+    { max: 5_000 },
+);
 
-function collectAuthors_noDuplicateRemoval(er: EducationalResource): LocalizedString[] {
-    if (!("parts" in er)) {
-        return er.authors;
-    }
+const collectAuthors_noDuplicateRemoval = memoize(
+    (er: EducationalResource): LocalizedString[] => {
+        if (!("parts" in er)) {
+            return er.authors;
+        }
 
-    return er.parts.map(collectAuthors_noDuplicateRemoval).flat();
-}
+        return er.parts.map(collectAuthors_noDuplicateRemoval).flat();
+    },
+);
 
-function collectTags(er: EducationalResource): EducationalResource.Tag[] {
+const collectTags = memoize((er: EducationalResource): EducationalResource.Tag[] => {
     if (!("parts" in er)) {
         return er.tags;
     }
@@ -205,9 +214,9 @@ function collectTags(er: EducationalResource): EducationalResource.Tag[] {
         .map(collectTags)
         .flat()
         .reduce(...removeDuplicates<EducationalResource.Tag>());
-}
+});
 
-function computeLastUpdatedTime(er: EducationalResource): number | undefined {
+const computeLastUpdatedTime = memoize((er: EducationalResource): number | undefined => {
     if (!("parts" in er)) {
         return er.lastUpdated === undefined
             ? undefined
@@ -220,63 +229,67 @@ function computeLastUpdatedTime(er: EducationalResource): number | undefined {
             .map(time => (time === undefined ? 0 : time))
             .reduce((prev, curr) => (curr > prev ? curr : prev), 0) || undefined
     );
-}
+});
 
-function computeTimeRequestedInMinutes(er: EducationalResource): number | undefined {
-    if (!("parts" in er)) {
-        return er.timeRequiredInMinutes === undefined
-            ? undefined
-            : er.timeRequiredInMinutes;
-    }
-
-    return (
-        er.parts
-            .map(computeTimeRequestedInMinutes)
-            .map(time => (time === undefined ? 0 : time))
-            .reduce((prev, curr) => curr + prev, 0) || undefined
-    );
-}
-
-function getAvailableInLanguages(params: {
-    educationalResource: EducationalResource;
-    languageAssumedIfNoTranslation: Language;
-}): Language[] {
-    const { educationalResource: er, languageAssumedIfNoTranslation } = params;
-
-    if (!("parts" in er)) {
-        const toLanguages = (ls: LocalizedString): Language[] => {
-            if (typeof ls === "string") {
-                return [];
-            }
-            return objectKeys(ls);
-        };
-
-        const candidates = [
-            ...(er.articleUrl === undefined ? [] : toLanguages(er.articleUrl)),
-            ...(er.deploymentUrl === undefined
-                ? []
-                : getIsLocalizedString(er.deploymentUrl)
-                  ? toLanguages(er.deploymentUrl)
-                  : Object.values(er.deploymentUrl).map(toLanguages).flat()),
-        ].reduce(...removeDuplicates<Language>());
-
-        if (candidates.length === 0) {
-            return [languageAssumedIfNoTranslation];
+const computeTimeRequestedInMinutes = memoize(
+    (er: EducationalResource): number | undefined => {
+        if (!("parts" in er)) {
+            return er.timeRequiredInMinutes === undefined
+                ? undefined
+                : er.timeRequiredInMinutes;
         }
 
-        return candidates;
-    }
+        return (
+            er.parts
+                .map(computeTimeRequestedInMinutes)
+                .map(time => (time === undefined ? 0 : time))
+                .reduce((prev, curr) => curr + prev, 0) || undefined
+        );
+    },
+);
 
-    return er.parts
-        .map(er =>
-            getAvailableInLanguages({
-                educationalResource: er,
-                languageAssumedIfNoTranslation,
-            }),
-        )
-        .flat()
-        .reduce(...removeDuplicates<Language>());
-}
+const getAvailableInLanguages = memoize(
+    (params: {
+        educationalResource: EducationalResource;
+        languageAssumedIfNoTranslation: Language;
+    }): Language[] => {
+        const { educationalResource: er, languageAssumedIfNoTranslation } = params;
+
+        if (!("parts" in er)) {
+            const toLanguages = (ls: LocalizedString): Language[] => {
+                if (typeof ls === "string") {
+                    return [];
+                }
+                return objectKeys(ls);
+            };
+
+            const candidates = [
+                ...(er.articleUrl === undefined ? [] : toLanguages(er.articleUrl)),
+                ...(er.deploymentUrl === undefined
+                    ? []
+                    : getIsLocalizedString(er.deploymentUrl)
+                      ? toLanguages(er.deploymentUrl)
+                      : Object.values(er.deploymentUrl).map(toLanguages).flat()),
+            ].reduce(...removeDuplicates<Language>());
+
+            if (candidates.length === 0) {
+                return [languageAssumedIfNoTranslation];
+            }
+
+            return candidates;
+        }
+
+        return er.parts
+            .map(er =>
+                getAvailableInLanguages({
+                    educationalResource: er,
+                    languageAssumedIfNoTranslation,
+                }),
+            )
+            .flat()
+            .reduce(...removeDuplicates<Language>());
+    },
+);
 
 export function getIsLocalizedString(arg: any): arg is LocalizedString {
     if (typeof arg === "string") {
