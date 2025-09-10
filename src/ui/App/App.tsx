0797a7023a4198c855f6cf4c96853410ce56a757
garronej <joseph.garrone@protonmail.com>
2025-09-10 05:40:08 +0200

    Remove screen scaller

--- src/ui/App/App.tsx
+++ src/ui/App/App.tsx
@@ -1,19 +1,7 @@
 import { useEffect, Suspense, lazy } from "react";
 import { OnyxiaUi } from "ui/theme";
-import { useSplashScreen, breakpointsValues } from "onyxia-ui";
+import { useSplashScreen } from "onyxia-ui";
 import { SuspenseFallback } from "./SuspenseFallback";
-import { enableScreenScaler } from "screen-scaler";
-
-enableScreenScaler({
-    targetWindowInnerWidth: ({ zoomFactor, actualWindowInnerWidth }) => {
-        if (actualWindowInnerWidth < breakpointsValues.xl) {
-            return actualWindowInnerWidth;
-        }
-
-        return (breakpointsValues.xl + 100) * zoomFactor;
-    },
-    rootDivId: "root",
-});
 
 const AppLazy = lazy(() => import("./App.lazy"));
 
