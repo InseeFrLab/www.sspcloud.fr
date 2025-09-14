3369e83d3a36ca80594706b46e72bb5dd0a378b8
garronej <joseph.garrone@protonmail.com>
2025-09-14 09:31:53 +0200

    Fix logical bug

--- src/core/usecases/_shared/thunks.ts
+++ src/core/usecases/_shared/thunks.ts
@@ -2,6 +2,8 @@ import type { Thunks } from "core/bootstrap";
 import { getCatalogData } from "core/adapters/catalogData";
 import { actions } from "./state";
 import { createUsecaseContextApi } from "clean-architecture";
+import { id } from "tsafe";
+import { Deferred } from "evt/tools/Deferred";
 
 export type RouteParams = {
     path?: string[] | undefined;
@@ -15,24 +17,26 @@ export const thunks = {
         async (...args): Promise<void> => {
             const [dispatch, , rootContext] = args;
 
-            {
-                const context = getContext(rootContext);
+            const context = getContext(rootContext);
 
-                if (context.hasLoaded) {
-                    return;
-                }
-
-                context.hasLoaded = true;
+            if (context.prLoaded !== undefined) {
+                return context.prLoaded;
             }
+
+            const dLoaded = new Deferred<void>();
+
+            context.prLoaded = dLoaded.pr;
 
             dispatch(
                 actions.loaded({
                     catalogData: await getCatalogData(),
                 }),
             );
+
+            dLoaded.resolve();
         },
 } satisfies Thunks;
 
 const { getContext } = createUsecaseContextApi(() => ({
-    hasLoaded: false,
+    prLoaded: id<Promise<void> | undefined>(undefined),
 }));
