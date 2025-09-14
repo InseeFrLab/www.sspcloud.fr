8a2c1425d631c093e0c401d6cf8d19b2c27ebcd7
garronej <joseph.garrone@protonmail.com>
2025-09-15 00:38:32 +0200

    Resize cartiflette

--- catalogData/tutoriels_de_cartographie_avec_cartiflette.ts
+++ catalogData/tutoriels_de_cartographie_avec_cartiflette.ts
@@ -1,19 +1,19 @@
 import type { EducationalResource } from "./-index";
-import cartiflette_png_url from "./-assets/cartiflette.png";
+import cartiflette_webp_url from "./-assets/cartiflette.webp";
 
 export const tutoriels_de_cartographie_avec_cartiflette: EducationalResource.Collection =
     {
         name: "Tutoriels de cartographie avec cartiflette",
         abstract:
             "Une série de tutoriels pour découvrir la manière dont cartiflette simplifie la récupération de fonds de carte de l'IGN pour la cartographie en Python",
-        imageUrl: cartiflette_png_url,
+        imageUrl: cartiflette_webp_url,
         parts: [
             {
                 name: "Créer une carte consolidée des arrondissements et des communes",
                 abstract:
                     "Un tutoriel pas à pas pour construire une carte de la densité de Velib dans la région francilienne",
                 authors: ["Lino Galiana"],
-                imageUrl: cartiflette_png_url,
+                imageUrl: cartiflette_webp_url,
                 tags: ["consolidate", "deepen", "Data Science Training", "Tutorial"],
                 articleUrl:
                     "https://inseefrlab.github.io/cartiflette-website/use-case/usecase1.html",
@@ -23,7 +23,7 @@ export const tutoriels_de_cartographie_avec_cartiflette: EducationalResource.Col
                 abstract:
                     "Un tutoriel pas à pas pour construire une carte comparant la taille des élevages à la population dans chaque département",
                 authors: ["Lino Galiana"],
-                imageUrl: cartiflette_png_url,
+                imageUrl: cartiflette_webp_url,
                 tags: ["consolidate", "deepen", "Data Science Training", "Tutorial"],
                 articleUrl:
                     "https://inseefrlab.github.io/cartiflette-website/use-case/usecase2.html",
